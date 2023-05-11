60fffa58aff6c927e1a3f64e67d8a58bb75a0885
songzilong <348320170@qq.com>
2023-05-11 21:04:50 +0800

    1

--- DAY7/second-vue/src/App.vue
+++ DAY7/second-vue/src/App.vue
@@ -1,29 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- 4. 局部组件使用 -->
-
-    <!-- 将 `msg` 作为 "实参"，传递给子组件的 "形参" - `name` -->
-    <AComp :name="msg" :age="155" />
-    <!-- 
-      <AComp :name="msg" age="十三岁" />
-      报错信息：Invalid prop: type check failed for prop "age". Expected Number with value NaN, got String with value "十三岁".
-      原因：age 参数只能接收数字，而实例传参 是字符串
-     -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-// 2. 引入局部组件
-import AComp from "@/components/AComp.vue"; // 路径中 `@` 直接表示 `/src` 文件夹
-
 export default {
   name: "App",
-  components: {
-    // HelloWorld,
-    AComp, // 3. 局部组件注册
-  },
+  components: {},
   data() {
     return {
       msg: "123-abc",
