5608308b351d553bc25fbcb3d7eeaf5f699999e1
songzilong <348320170@qq.com>
2023-05-11 20:34:26 +0800

    1

---
+++ DAY7/second-vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- 4. 局部组件使用 -->
+    <AComp />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+// 2. 引入局部组件
+import AComp from "@/components/AComp.vue"; // 路径中 `@` 直接表示 `/src` 文件夹
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    AComp, // 3. 局部组件注册
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ DAY7/second-vue/src/components/AComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    <h1>我是 A 组件</h1>
+  </div>
+</template>
+
+<script>
+// 1. 组件声明
+export default {
+  name: "AComp", // 给组件起一个 名字/别名
+};
+</script>
